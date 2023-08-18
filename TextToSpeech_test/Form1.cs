cd71fa71cf57ef0b1590a21212fc3da0e5a3a46a
Віталій Вітровський <vitalikvitrovskij@gmail.com>
2023-08-18 11:45:34 +0300

    Добавьте файлы проекта.

---
+++ TextToSpeech_test/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace TextToSpeech_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+            synthesizer.Volume = 100;  // 0...100
+            synthesizer.Rate = -2;     // -10...10
+
+            // Synchronous
+            //synthesizer.Speak("Hello World");
+
+            // Asynchronous
+            synthesizer.SpeakAsync("");
+
+        }
+    }
+}
